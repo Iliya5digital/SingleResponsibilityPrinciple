1f64e2116b9878aea1a1d506184dc19ad01bdcf3
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-04-10 14:45:15 +0300

    Add files via upload

---
+++ Students/Old/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SingleResponsibilityPrinciple.Students.Old
+{
+    //сущность "студент"
+    //нарушение принципа Single Responsibility Principle
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int GroupId { get; set; }
+
+        //выбрать всех студентов из БД
+        public static IEnumerable<Student> GetAllStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        //выбрать конкретного студента по Id из БД
+        public static Student GetStudentById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        //выбрать всех студентов в определенной группе из БД
+        public static IEnumerable<Student> GetStudentsByGroupId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
