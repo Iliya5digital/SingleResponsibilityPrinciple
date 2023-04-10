55179aa39a9a446e57fab723f4050b5ab0f4fa42
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-04-10 14:44:46 +0300

    Add files via upload

---
+++ Students/New/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace SingleResponsibilityPrinciple.Students.New
+{
+    //сущность "студент"
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int GroupId { get; set; }
+    }
+}
---
+++ Students/New/StudentsRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SingleResponsibilityPrinciple.Students.New
+{
+    //репозиторий студентов
+    //ответственность класса - работа с базой данных
+    public class StudentsRepository
+    {
+        //выбрать всех студентов
+        public IEnumerable<Student> GetAllStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        //выбрать конкретного студента по Id
+        public Student GetStudentById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        //выбрать всех студентов в определенной группе
+        public IEnumerable<Student> GetStudentsByGroupId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
